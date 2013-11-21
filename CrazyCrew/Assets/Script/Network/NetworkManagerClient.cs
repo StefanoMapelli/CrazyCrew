648932c0cb1d3e1fea6a8319a54c727c91f0f27d
StefanoMapelli <stefano5.mapelli@mail.polimi.it>
2013-11-21 11:28:29 +0100

    First commit
    Server amd client scenes created

---
+++ CrazyCrew/Assets/Script/Network/NetworkManagerClient.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+public class NetworkManagerClient : MonoBehaviour {
+	
+	private UdpClient udpClient;
+	private int serverPort;
+	private string serverIpAddress;
+	private bool messageReceived=false;
+
+	// Use this for initialization
+	void Start () {
+		
+		udpClient = new UdpClient(9050);
+		System.Threading.Thread connection_thread = new System.Threading.Thread(WaitForServer);
+		connection_thread.Start ();
+	}
+	
+	void WaitForServer()
+	{
+		//IPEndPoint object will allow us to read datagrams sent from any source on port 9050.
+		IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 9050);
+		
+		// Receive message from server and control if it's the CrazyCrew message.
+		while(!messageReceived)
+		{
+			Byte[] receiveBytes = udpClient.Receive(ref RemoteIpEndPoint); 
+			string returnData = Encoding.ASCII.GetString(receiveBytes);
+			if(returnData.CompareTo("CrazyCrewServer: HELLO")==0)
+			{
+				messageReceived=true;
+			}
+		}
+		
+		serverIpAddress=RemoteIpEndPoint.Address.ToString();
+		serverPort= RemoteIpEndPoint.Port;
+		
+		Debug.Log(serverIpAddress + serverPort);
+		
+		udpClient.Close();
+	}
+	
+	void OnGUI()
+	{
+		if(messageReceived)
+		{
+			if(GUI.Button(new Rect(100,100,100,100),"Connect To server"))
+			{
+				Network.Connect(serverIpAddress,25000);
+				
+			}
+		}
+		else
+			GUI.Label(new Rect(200,100,100,100),"not connected");
+		
+		if(Network.isClient){
+			GUI.Label(new Rect(100,200,100,100),"connect to server with network");
+			
+			if(GUI.Button(new Rect(200,200,100,100),"Brake")){
+				//Ho premuto il freno dx
+				
+				GameObject controllerBogie = GameObject.Find ("ControllerBogieCar");
+				Debug.Log(controllerBogie.ToString());
+				//controllerBogie.networkView.RPC ("pressRightBrake",RPCMode.Server,null);
+			}
+		}
+			
+	}
+	
+	void OnConnectedToServer()
+	{
+		//Devo ricevere il messaggio di quale controller visualizzare
+		//Application.LoadLevel ("BogieRight");
+	}
+	
+	void OnDisconnectedFromServer()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
