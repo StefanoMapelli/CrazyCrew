4209e8ac32a0704e072883b0d049d05b8e66e120
dario90 <navoni.dario@tiscali.it>
2014-01-10 11:13:08 +0100

    bug fixing

--- CrazyCrew/Assets/Script/GUI/ClientGUI/RestartButton.cs
+++ CrazyCrew/Assets/Script/GUI/ClientGUI/RestartButton.cs
@@ -4,16 +4,27 @@ using System.Collections;
 public class RestartButton : MonoBehaviour {
 
 	private ClientGameManager clientGameManager;
+	private PowerUpController powerUpController;
 	
 	// Use this for initialization
 	void Start () {
 		clientGameManager = (ClientGameManager) GameObject.Find ("Client").GetComponent("ClientGameManager");
+		powerUpController = (PowerUpController) GameObject.Find ("PowerUpButton").GetComponent("PowerUpController");
 	}
 
 	void OnMouseDown() {
 		((TextMesh)gameObject.GetComponent("TextMesh")).color = Color.black;
 		clientGameManager.restart();
 		clientGameManager.setPause(false);
+
+		// eliminate old bonus
+		powerUpController.setHasBonus(false);
+		powerUpController.updateBonusLabel();
+
+		// eliminate old malus
+		powerUpController.setHasMalus(false);
+		powerUpController.updateMalusLabel();
+
 	}
 
 	void OnMouseUp()
